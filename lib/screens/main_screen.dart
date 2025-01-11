ca964a2d9a3bb5fb52cd4f5de723e4545dc739aa
Vuk Bundalo <vukbundalo777@gmail.com>
2025-01-11 23:25:03 +0100

    Added pharmacy filter

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,7 +4,6 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'settings_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../utils/constants.dart';
 import '../widgets/prescription_card.dart';
 
 class MainScreen extends StatefulWidget {
@@ -20,30 +19,38 @@ class MainScreenState extends State<MainScreen> {
   DateTime selectedDate = DateTime.now();
   List<dynamic> prescriptions = [];
   String searchQuery = "";
-  bool isLoading = false; // Track loading state
-  String errorMessage = ""; // Track error message
-  String searchQueryCode = ""; // For searching by "Fond šifra lijeka"
-  String searchQueryRecept = ""; // For searching by "broj recepta"
+  bool isLoading = false;
+  String errorMessage = "";
+  String searchQueryCode = "";
+  String searchQueryRecept = "";
+
+  String? selectedPharmacy = "Sve apoteke"; // Default option
+  List<String> pharmacyList = ["Sve apoteke"]; // Includes "Sve apoteke"
 
   @override
   void initState() {
     super.initState();
     loadSettings();
-    checkForUpdates(); // Automatically check for updates on startup
+    checkForUpdates();
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    loadSettings(); // Reload settings whenever the screen is rebuilt
+    loadSettings();
   }
 
   Future<void> loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
-    if (!mounted) return; // Ensure the widget is still in the tree
+    if (!mounted) return;
     setState(() {
       pLicence = prefs.getString('p_licence') ?? "";
       pMainOrgId = prefs.getString('p_main_org_id') ?? "";
+
+      // Reset pharmacy list and selected pharmacy
+      prescriptions = [];
+      pharmacyList = ["Sve apoteke"];
+      selectedPharmacy = "Sve apoteke";
     });
   }
 
@@ -52,24 +59,15 @@ class MainScreenState extends State<MainScreen> {
         "https://raw.githubusercontent.com/vukbundalo/fond_check/refs/heads/main/latest_version.json";
 
     try {
-      // Fetch the latest version information
       final response = await http.get(Uri.parse(versionCheckUrl));
       if (response.statusCode == 200) {
         final Map<String, dynamic> latestVersionData =
             json.decode(response.body);
-
-        // Parse version and download URL
         final String latestVersion = latestVersionData["version"];
         final String downloadUrl = latestVersionData["url"];
-
-        // Compare versions
-        if (latestVersion != currentAppVersion) {
-          // Show dialog to notify the user about the update
-          if (!mounted) return;
+        if (latestVersion != "1.0.0") {
           showUpdateDialog(latestVersion, downloadUrl);
         }
-      } else {
-        throw Exception("Failed to fetch version data");
       }
     } catch (e) {
       if (mounted) {
@@ -81,26 +79,43 @@ class MainScreenState extends State<MainScreen> {
   }
 
   void showUpdateDialog(String latestVersion, String downloadUrl) {
+    if (!mounted) return; // Ensure the widget is mounted before using context
+
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
+      context: context, // Use the current context directly here
+      builder: (BuildContext dialogContext) {
+        // The dialog's context is captured here
         return AlertDialog(
           title: const Text("Nova verzija dostupna"),
           content: Text(
-            "Dostupna je nova verzija aplikacije ($latestVersion). "
-            "Želite li preuzeti novu verziju?",
+            "Dostupna je nova verzija aplikacije ($latestVersion). Želite li preuzeti novu verziju?",
           ),
           actions: [
             TextButton(
               onPressed: () {
-                Navigator.pop(context); // Close the dialog
+                if (Navigator.of(dialogContext).canPop()) {
+                  Navigator.pop(dialogContext); // Close the dialog
+                }
               },
               child: const Text("Kasnije"),
             ),
             ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context); // Close the dialog
-                openUrl(downloadUrl);
+              onPressed: () async {
+                if (Navigator.of(dialogContext).canPop()) {
+                  Navigator.pop(dialogContext); // Close the dialog
+                }
+                final Uri uri = Uri.parse(downloadUrl);
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri, mode: LaunchMode.externalApplication);
+                } else {
+                  if (mounted) {
+                    // Re-check if the widget is mounted
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content: Text("Ne mogu otvoriti URL: $downloadUrl")),
+                    );
+                  }
+                }
               },
               child: const Text("Preuzmi"),
             ),
@@ -108,21 +123,6 @@ class MainScreenState extends State<MainScreen> {
         );
       },
     );
-  }
-
-  Future<void> openUrl(String url) async {
-    final Uri uri = Uri.parse(url); // Parse the URL into a Uri object
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(
-        uri,
-        mode: LaunchMode.externalApplication, // Specify launch mode
-      );
-    } else {
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Ne mogu otvoriti URL: $url")),
-      );
-    }
   }
 
   Future<void> fetchPrescriptions() async {
@@ -142,8 +142,8 @@ class MainScreenState extends State<MainScreen> {
     }
 
     setState(() {
-      isLoading = true; // Show loading indicator
-      errorMessage = ""; // Reset error message
+      isLoading = true;
+      errorMessage = "";
     });
 
     final date =
@@ -153,21 +153,24 @@ class MainScreenState extends State<MainScreen> {
 
     try {
       final response = await http.get(Uri.parse(url));
-      if (!mounted) return; // Ensure the widget is still in the tree
-
       if (response.statusCode == 200) {
+        final data = json.decode(response.body)["items"];
         setState(() {
-          prescriptions = json.decode(response.body)["items"];
-          isLoading = false; // Hide loading indicator
+          prescriptions = data;
+          pharmacyList = ["Sve apoteke"] +
+              prescriptions
+                  .map((item) => item["pj_apoteka"] as String)
+                  .toSet()
+                  .toList();
+          isLoading = false;
         });
       } else {
         throw Exception("Učitavanje podataka nije uspijelo.");
       }
     } catch (e) {
       setState(() {
-        isLoading = false; // Hide loading indicator
-        errorMessage =
-            "Konekcija sa serverom nije uspijela. Provjerite internet konekciju i pokušajte ponovo.";
+        isLoading = false;
+        errorMessage = "Konekcija sa serverom nije uspijela.";
       });
     }
   }
@@ -179,23 +182,12 @@ class MainScreenState extends State<MainScreen> {
       firstDate: DateTime(2000),
       lastDate: DateTime(2100),
     );
-    if (!mounted) return; // Ensure the widget is still in the tree
+    if (!mounted) return;
     if (pickedDate != null && pickedDate != selectedDate) {
       setState(() {
         selectedDate = pickedDate;
       });
     }
-  }
-
-  // Helper function to get pharmacy name by license
-  String? getPharmacyNameByLicense(String? license) {
-    if (license == null || license.isEmpty) return null;
-    for (final pharmacy in pharmacies) {
-      if (pharmacy["license"] == license) {
-        return pharmacy["name"];
-      }
-    }
-    return "Nepoznata apoteka"; // Default if license doesn't match
   }
 
   @override
@@ -209,8 +201,6 @@ class MainScreenState extends State<MainScreen> {
           .toString()
           .toLowerCase()
           .contains(searchQueryCode.toLowerCase());
-
-      // Extract the number after the last dot in "ihe_oznaka"
       final iheMatch = item["ihe_oznaka"] != null
           ? item["ihe_oznaka"]
               .toString()
@@ -218,64 +208,23 @@ class MainScreenState extends State<MainScreen> {
               .last
               .contains(searchQueryRecept)
           : false;
-
-      return nameMatch && codeMatch && iheMatch; // All conditions must be true
+      final pharmacyMatch = selectedPharmacy == "Sve apoteke" ||
+          item["pj_apoteka"] == selectedPharmacy;
+      return nameMatch && codeMatch && iheMatch && pharmacyMatch;
     }).toList();
-    final pharmacyName =
-        getPharmacyNameByLicense(pLicence); // Get pharmacy name
+
     return Scaffold(
       appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ShaderMask(
-              shaderCallback: (bounds) => const LinearGradient(
-                colors: [Colors.blue, Colors.lightBlueAccent],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ).createShader(bounds),
-              child: const Text(
-                "FondCheck1.0",
-                style: TextStyle(
-                  fontSize: 24, // Larger font size for the title
-                  fontWeight: FontWeight.bold, // Bold text for emphasis
-                  fontFamily: 'Serif', // Elegant font
-                  color: Colors.white, // Gradient applies here
-                  letterSpacing: 1.5, // Slight letter spacing for premium feel
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            const SizedBox(
-                height: 4), // Small spacing between title and subtitle
-            const Text(
-              "Pregled elektronskih recepata koji su izdati u fond",
-              style: TextStyle(
-                fontSize: 14, // Smaller font size for subtitle
-                fontWeight: FontWeight.w400, // Normal font weight
-                color: Colors.white, // White color for subtitle
-                letterSpacing: 1.0, // Slight letter spacing for readability
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-        centerTitle: true, // Center the title and subtitle
-        backgroundColor:
-            Colors.blue.shade900, // Dark blue background for pharmacy theme
+        title: const Text("FondCheck1.0"),
         actions: [
           IconButton(
-            icon: const Icon(Icons.settings, color: Colors.white),
+            icon: const Icon(Icons.settings),
             onPressed: () async {
-              // Navigate to the Settings Page
               await Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const SettingsScreen()),
               );
-              // Reload settings after returning
-              if (mounted) {
-                await loadSettings();
-              }
+              await loadSettings(); // Reload settings and reset dropdown
             },
           ),
         ],
@@ -291,13 +240,22 @@ class MainScreenState extends State<MainScreen> {
                 Text(
                   "Recepti za datum: ${selectedDate.day}.${selectedDate.month}.${selectedDate.year}",
                   style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
+                      fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                if (pLicence != null && pLicence!.isNotEmpty)
-                  Text(
-                    "$pharmacyName",
-                    style: const TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
+                DropdownButton<String>(
+                  value: selectedPharmacy,
+                  items: pharmacyList
+                      .map((pharmacy) => DropdownMenuItem(
+                            value: pharmacy,
+                            child: Text(pharmacy),
+                          ))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedPharmacy = value;
+                    });
+                  },
+                ),
               ],
             ),
             const SizedBox(height: 16),
@@ -338,8 +296,7 @@ class MainScreenState extends State<MainScreen> {
               decoration: const InputDecoration(labelText: "Broj recepta"),
               onChanged: (value) {
                 setState(() {
-                  searchQueryRecept =
-                      value; // Update search query for "broj recepta"
+                  searchQueryRecept = value;
                 });
               },
             ),
@@ -347,50 +304,26 @@ class MainScreenState extends State<MainScreen> {
             Expanded(
               child: isLoading
                   ? const Center(
-                      child: CircularProgressIndicator(color: Colors.blue))
+                      child: CircularProgressIndicator(),
+                    )
                   : errorMessage.isNotEmpty
                       ? Center(
                           child: Text(
                             errorMessage,
-                            style: const TextStyle(
-                                fontSize: 18, color: Colors.red),
-                            textAlign: TextAlign.center,
+                            style: const TextStyle(color: Colors.red),
                           ),
                         )
                       : filteredPrescriptions.isEmpty
                           ? const Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    "Nema dostupnih recepata za prikaz.",
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.grey),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    "Izaberite datum i kliknite na dugme \"Preuzmi recepte sa fond servera\".",
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.grey),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                  SizedBox(height: 8),
-                                  Text(
-                                    "Postarajte se da ste unijeli licencu i kod apoteke u podešavanjima programa (Kotačić u gornjem desnom uglu) ako to nikada do sada niste uradili.",
-                                    style: TextStyle(
-                                        fontSize: 18, color: Colors.grey),
-                                    textAlign: TextAlign.center,
-                                  ),
-                                ],
-                              ),
-                            )
+                              child: Text("Nema dostupnih recepata."))
                           : ListView.builder(
                               itemCount: filteredPrescriptions.length,
                               itemBuilder: (context, index) {
                                 final item = filteredPrescriptions[index];
                                 return PrescriptionCard(
-                                    item: item, selectedDate: selectedDate);
+                                  item: item,
+                                  selectedDate: selectedDate,
+                                );
                               },
                             ),
             ),
